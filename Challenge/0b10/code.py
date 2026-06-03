0e0879f6f36138c67556022b4201ad0b493e1eea
Schrodinger's Bug <kordonouris.nikos@gmail.com>
2026-06-03 10:49:12 +0300

    Uploaded challenge

---
+++ Challenge/0b10/code.py
@@ -0,0 +1,8 @@
+code = 1215
+
+flag_code = int(input("What is the code? "))
+
+if flag_code == code:
+    print("The flag is vincin.")
+else:
+    print("Incorrect.")
---
+++ Challenge/0b11/flagtest.py
@@ -0,0 +1,8 @@
+flag = "vincin"
+
+check = input("What is the flag?")
+
+if check == flag:
+    print("Correct!Challenge solved!")
+elif check != flag:
+    print("Incorrect.")
